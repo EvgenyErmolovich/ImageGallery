cdc32a083a2ce7ac514ee5fc7a167bff71755bcc
Evgeny <evga.1998@mail.ru>
2017-10-02 19:33:18 +0300

    Code

---
+++ ImageGallery/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ImageGallery.Models;
+using System.IO;
+
+namespace ImageGallery.Controllers
+{
+    public class HomeController : Controller
+    {
+        ImageEntitie context = new ImageEntitie();
+        
+        [HttpGet]
+        public FileContentResult GetImage(int imageId)
+        { 
+            Image image = context.Image.FirstOrDefault(i => i.Id == imageId);
+            if(image != null) return File(image.ImageData, image.ImageMimeType);
+            return null;
+        }
+    }
+}
